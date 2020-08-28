3261069ef431596143a3126c18b28df1bfa5d845
Pond-Dev <sanhapas.arun@gmail.com>
2020-08-28 17:34:06 +0700

    setup

---
+++ project/Profile/src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer id="footer" className="bg-dark text-white mt-5 p-2 text-center">
+          {/* Profile {new Date().getFullYear()} */}
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Footer;
