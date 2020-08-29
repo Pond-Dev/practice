7c4c88c091edcebbd4d7a17312ca8ffbda0b890f
Pond-Dev <sanhapas.arun@gmail.com>
2020-08-29 21:56:28 +0700

    fix-to-devconnector

---
+++ project/devconnector/client/src/components/auth/Register.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+
+export class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      password2: "",
+    };
+  }
+
+  onChange(event) {
+    this.setState({ [event.target.name]: event.currentTarget.value });
+  }
+  onSubmit(event) {
+    event.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2,
+    };
+    console.log(newUser);
+  }
+  render() {
+    return (
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">
+                Create your DevConnector account
+              </p>
+              <form onSubmit={this.onSubmit.bind(this)}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Name"
+                    name="name"
+                    required
+                    onChange={this.onChange.bind(this)}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    onChange={this.onChange.bind(this)}
+                  />
+                  <small className="form-text text-muted">
+                    This site uses Gravatar so if you want a profile image, use
+                    a Gravatar email
+                  </small>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    onChange={this.onChange.bind(this)}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Confirm Password"
+                    name="password2"
+                    onChange={this.onChange.bind(this)}
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Register;
---
+++ project/devconnector/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://pond:arun@devconnect.75xir.mongodb.net/<project 0>?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
