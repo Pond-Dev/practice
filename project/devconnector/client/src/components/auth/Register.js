5db31f7201c68da6815ac4425547af9b026408d2
Pond-Dev <sanhapas.arun@gmail.com>
2020-09-02 15:05:11 +0700

    add-backend

--- project/devconnector/client/src/components/auth/Register.js
+++ project/devconnector/client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export class Register extends Component {
   constructor(props) {
@@ -23,7 +24,10 @@ export class Register extends Component {
       password: this.state.password,
       password2: this.state.password2,
     };
-    console.log(newUser);
+    axios
+      .post("/api/users/register", newUser)
+      .then((res) => console.log(res.data))
+      .catch((err) => this.setState({ errors: err.response.data }));
   }
   render() {
     return (
---
+++ project/devconnector/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://pond:arun@devconnect-shard-00-00.75xir.mongodb.net:27017,devconnect-shard-00-01.75xir.mongodb.net:27017,devconnect-shard-00-02.75xir.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-p3xik7-shard-0&authSource=admin&retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
